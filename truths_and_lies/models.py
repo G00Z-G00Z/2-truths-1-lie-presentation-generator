3e64f54d5d06eaa03a0ba2a8da0534b83f4483b2
G00Z_G00Z <edygomezgg4@gmail.com>
2023-12-08 14:04:10 -0600

    Added method for the list of participants

--- truths_and_lies/models.py
+++ truths_and_lies/models.py
@@ -31,3 +31,25 @@ class ListOfParticipants(BaseModel):
     """
 
     participants: list[Participant]
+
+    def find_participant(self, name: str) -> Participant | None:
+        """
+        Find a participant by name.
+        """
+        for participant in self.participants:
+            if participant.name == name:
+                return participant
+
+        return None
+
+    def add_participant(self, participant: Participant) -> None:
+        """
+        Add a participant to the list of participants.
+        """
+        self.participants.append(participant)
+
+    def remove_participant(self, name: str) -> None:
+        """
+        Remove a participant from the list of participants.
+        """
+        self.participants = [p for p in self.participants if p.name != name]
