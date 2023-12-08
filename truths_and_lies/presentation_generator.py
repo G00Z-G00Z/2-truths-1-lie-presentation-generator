290812501db2f5665f479027162d8edf35aba2d9
G00Z_G00Z <edygomezgg4@gmail.com>
2023-12-08 13:48:19 -0600

    Presentation generator

---
+++ truths_and_lies/presentation_generator.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from pptx import Presentation
+from pptx.dml.color import RGBColor
+from .json_parser import parse_participants
+
+
+def create_presentation(
+    participant_path: Path,
+    presentation_path: Path,
+) -> None:
+    participants = parse_participants(participant_path)
+    prs = Presentation()
+    for participant in participants.participants:
+        slide = prs.slides.add_slide(prs.slide_layouts[1])
+        title = slide.shapes.title
+        title.text = f"Statements from {participant.name}"
+        content = slide.placeholders[1]
+        content.text = ""
+        for statement in participant.statements:
+            color = RGBColor(0, 128, 0) if not statement.isLie else RGBColor(255, 0, 0)
+            p = content.text_frame.add_paragraph()
+            p.text = statement.statement
+            p.font.color.rgb = color
+
+    prs.save(presentation_path)
